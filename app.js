ffd203f9409402ede6bf4cce46e5e2ebc49a44a5
Jeremy Su <jeremysu64@gmail.com>
2022-09-09 15:36:35 -0400

    update

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 //oo wee oo... wee
+function scrollToAnchor(aid){
+    console.log('yo');
+    const destination = $("a[name='"+ aid +"']");
+      $("#destination").animate({
+        scrollTop: destination.offset().top
+      },'slow');
+}
+
+$(document).on("click", "entrance", function(){
+  scrollToAnchor('#destination');
+})
+
+function empty(){
+    /*make smaller */
+    console.log('hit');
+}
